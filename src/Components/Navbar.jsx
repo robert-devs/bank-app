b2b4ce27fff50bfcc7bbdde694f2f53c20ff4bf6
robert-devs <mainrobert04@gmail.com>
2023-03-07 12:27:42 +0300

    adding the assets and the components layout

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+    </nav>
+  )
+}
+
+export default Navbar
