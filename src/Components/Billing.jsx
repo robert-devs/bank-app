40222ee55b11c0aed04faa7791535a2085b20329
robert-devs <mainrobert04@gmail.com>
2023-03-07 18:43:08 +0300

    adding the card and billing sections

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { apple, bill, google } from '../assets'
+import styles, { layout } from '../style'
 
-const Billing = () => {
-  return <div>Billing</div>
-}
+const Billing = () => (
+  <section id="product" className={layout.sectionImgReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt=" bill image"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+    </div>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily Control Your <br className="sm:block hidden" /> Billing &
+        Invoicing
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean
+        neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+      <div className="flex flex-row mt-6 flex-wrap sm:mt-10">
+        <img
+          src={apple}
+          alt="apple store"
+          className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+        />
+        <img
+          src={google}
+          alt="google play store"
+          className="w-[128px] h-[42px] object-contain cursor-pointer"
+        />
+      </div>
+    </div>
+  </section>
+)
 
 export default Billing
--- src/Components/CardDeal.jsx
+++ src/Components/CardDeal.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-  return <div>CardDeal</div>
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        {' '}
+        Find a a better card deal <br className="sm:block hidden" /> in few easy
+        steps{' '}
+      </h2>
+      <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+        Arcu tortor, purus in mattis <br className="sm:block hidden" /> at sed
+        integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet
+        ultrices ac, ametau.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt=" card image" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+)
 
 export default CardDeal
