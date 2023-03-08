fa0d31bde413aa96420d4fb896a711615c89e98f
robert-devs <mainrobert04@gmail.com>
2023-03-08 11:48:03 +0300

    adding the client section

--- src/Components/Clients.jsx
+++ src/Components/Clients.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../style'
 
-const Clients = () => {
-  return <div>Clients</div>
-}
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client, index) => (
+        <div
+          key={client.id}
+          className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={client.logo}
+            alt="clients logo"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+)
 
 export default Clients
